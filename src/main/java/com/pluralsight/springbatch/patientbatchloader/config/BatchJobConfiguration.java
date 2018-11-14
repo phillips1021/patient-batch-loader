6d04f28c2209f956684291d316b10766d4f98ff8
brucephillips <bphillips@ku.edu>
2018-11-14 06:18:33 -0600

    more changes as I progress through the pluralsight course

--- src/main/java/com/pluralsight/springbatch/patientbatchloader/config/BatchJobConfiguration.java
+++ src/main/java/com/pluralsight/springbatch/patientbatchloader/config/BatchJobConfiguration.java
@@ -1,5 +1,6 @@
 package com.pluralsight.springbatch.patientbatchloader.config;
 
+import com.pluralsight.springbatch.patientbatchloader.domain.PatientRecord;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.batch.core.*;
 import org.springframework.batch.core.configuration.JobRegistry;
@@ -12,11 +13,25 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.batch.item.ItemReader;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.core.io.PathResource;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.support.PassThroughItemProcessor;
+import org.springframework.batch.item.ItemWriter;
+
+
 
 @Configuration
 public class BatchJobConfiguration {
@@ -75,18 +90,63 @@ public class BatchJobConfiguration {
     }
 
     @Bean
-    public Step step() throws Exception {
+    @StepScope
+    public FlatFileItemReader<PatientRecord> reader(
+        @Value("#{jobParameters['" + Constants.JOB_PARAM_FILE_NAME + "']}")String fileName) {
+        return new FlatFileItemReaderBuilder<PatientRecord>()
+            .name(Constants.ITEM_READER_NAME)
+            .resource(
+                new PathResource(
+                    Paths.get(applicationProperties.getBatch().getInputPath() +
+                        File.separator + fileName)))
+            .linesToSkip(1)
+            .lineMapper(lineMapper())
+            .build();
+    }
+
+    @Bean
+    public Step step(ItemReader<PatientRecord> itemReader) throws Exception {
         return this.stepBuilderFactory
             .get(Constants.STEP_NAME)
-            .tasklet(new Tasklet() {
-                @Override
-                public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext)
-                    throws Exception {
-                    System.err.println("Hello World!");
-                    return RepeatStatus.FINISHED;
-                }
-            })
+            .<PatientRecord, PatientRecord>chunk(2)
+            .reader(itemReader)
+            .processor(processor())
+            .writer(writer())
             .build();
+    }
+
+    @Bean
+    public LineMapper<PatientRecord> lineMapper() {
+        DefaultLineMapper<PatientRecord> mapper = new DefaultLineMapper<>();
+        mapper.setFieldSetMapper((fieldSet) -> new PatientRecord(
+            fieldSet.readString(0), fieldSet.readString(1),
+            fieldSet.readString(2), fieldSet.readString(3),
+            fieldSet.readString(4), fieldSet.readString(5),
+            fieldSet.readString(6), fieldSet.readString(7),
+            fieldSet.readString(8), fieldSet.readString(9),
+            fieldSet.readString(10), fieldSet.readString(11),
+            fieldSet.readString(12)));
+        mapper.setLineTokenizer(new DelimitedLineTokenizer());
+        return mapper;
+    }
+
+    @Bean
+    @StepScope
+    public PassThroughItemProcessor<PatientRecord> processor() {
+        return new PassThroughItemProcessor<>();
+    }
+
+    @Bean
+    @StepScope
+    public ItemWriter<PatientRecord> writer() {
+        return new ItemWriter<PatientRecord>() {
+            @Override
+            public void write(List<? extends PatientRecord> items) throws Exception {
+                for (PatientRecord patientRecord : items) {
+                    System.err.println("Writing item: " + patientRecord.toString());
+                }
+            }
+        };
     }
 
 }
